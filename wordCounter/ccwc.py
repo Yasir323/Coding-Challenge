160238a73396eecb37b23e8dfd5774f3364c8c1a
Yasir323 <yasir.jafri007@gmail.com>
2023-10-30 17:07:25 +0000

    Pipe as standard input implemented in word counter

--- wordCounter/ccwc.py
+++ wordCounter/ccwc.py
@@ -2,84 +2,6 @@ import argparse
 import sys
 
 
-def parse_optional_arguments(options, parser):
-    for option in options:
-        parser.add_argument(
-            option["short_option"],
-            option["verbose_option"],
-            action="store_true",
-            help=option["help"]
-        )
-
-
-def parse_positional_arguments(parser):
-    parser.add_argument("file")
-
-
-def parse_command_line_arguments(options):
-    parser = argparse.ArgumentParser()
-    parse_optional_arguments(options, parser)
-    parse_positional_arguments(parser)
-    args = parser.parse_args()
-    return args
-
-
-def get_number_of_bytes(fp):
-    fp.seek(0, 2)
-    total_bytes = fp.tell()
-    return total_bytes
-
-
-def get_number_of_lines(fp):
-    total_lines = 0
-    for _ in fp.readlines():
-        total_lines += 1
-    return total_lines
-
-
-def get_number_of_words(fp):
-    total_words = 0
-    for line in fp.readlines():
-        total_words += len(line.split())
-    return total_words
-
-
-def get_number_of_characters(fp):
-    data = fp.read()
-    total_chars = len(data.encode(sys.getfilesystemencoding()))
-    return total_chars
-
-
-def print_all_values(fp, filename):
-    total_lines = get_number_of_lines(fp)
-    fp.seek(0)
-    total_words = get_number_of_words(fp)
-    total_bytes = get_number_of_bytes(fp)
-    max_len = len(str(total_bytes))
-    print(f"{total_lines:>{max_len}} {total_words:>{max_len}} {total_bytes} {filename}")
-
-
-def print_results(arguments):
-    try:
-        with open(arguments.file, encoding="utf-8") as fp:
-            if arguments.bytes:
-                total_bytes = get_number_of_bytes(fp)
-                print(f"{total_bytes} {arguments.file}")
-            elif arguments.chars:  # Incorrect
-                total_chars = get_number_of_characters(fp)
-                print(f"{total_chars} {arguments.file}")
-            elif arguments.lines:
-                total_lines = get_number_of_lines(fp)
-                print(f"{total_lines} {arguments.file}")
-            elif arguments.words:
-                total_words = get_number_of_words(fp)
-                print(f"{total_words} {arguments.file}")
-            else:
-                print_all_values(fp, arguments.file)
-    except FileNotFoundError:
-        print("File not found: {}".format(arguments.file))
-
-
 def main():
     options = [
         {
@@ -106,6 +28,92 @@ def main():
     arguments = parse_command_line_arguments(options)
 
     print_results(arguments)
+
+
+def parse_command_line_arguments(options):
+    parser = argparse.ArgumentParser()
+    parse_optional_arguments(options, parser)
+    parse_positional_arguments(parser)
+    args = parser.parse_args()
+    return args
+
+
+def parse_optional_arguments(options, parser):
+    for option in options:
+        parser.add_argument(
+            option["short_option"],
+            option["verbose_option"],
+            action="store_true",
+            help=option["help"]
+        )
+
+
+def parse_positional_arguments(parser: argparse.ArgumentParser):
+    parser.add_argument("file", nargs="?")
+
+
+def print_results(arguments):
+    if arguments.file:
+        stream = open(arguments.file, encoding="utf-8")
+    else:
+        stream = sys.stdin
+    try:
+        if arguments.bytes:
+            total_bytes = get_number_of_bytes(stream)
+            print(f"{total_bytes} {arguments.file if arguments.file else ''}")
+        elif arguments.chars:  # Incorrect
+            total_chars = get_number_of_characters(stream)
+            print(f"{total_chars} {arguments.file if arguments.file else ''}")
+        elif arguments.lines:
+            total_lines = get_number_of_lines(stream)
+            print(f"{total_lines} {arguments.file if arguments.file else ''}")
+        elif arguments.words:
+            total_words = get_number_of_words(stream)
+            print(f"{total_words} {arguments.file if arguments.file else ''}")
+        else:
+            print_all_values(stream, arguments.file)
+    except FileNotFoundError:
+        print("File not found: {}".format(arguments.file))
+    finally:
+        stream.close()
+
+
+def get_number_of_bytes(stream):
+    data = stream.read()
+    total_bytes = len(data)
+    return total_bytes
+
+
+def get_number_of_characters(stream):
+    data = stream.read()
+    total_chars = len(data.encode(sys.getfilesystemencoding()))
+    return total_chars
+
+
+def get_number_of_lines(stream):
+    total_lines = 0
+    for _ in stream.readlines():
+        total_lines += 1
+    return total_lines
+
+
+def get_number_of_words(stream):
+    total_words = 0
+    for line in stream.readlines():
+        total_words += len(line.split())
+    return total_words
+
+
+def print_all_values(stream, filename):
+    data: str = stream.read()
+    lines = data.replace("\r\n", "\n").replace("\r", "\n").split("\n")
+    total_lines = len(lines) - 1
+    total_words = 0
+    for line in lines:
+        total_words += len(line.split())
+    total_bytes = len(data)
+    max_len = len(str(total_bytes))
+    print(f"{total_lines:>{max_len}} {total_words:>{max_len}} {total_bytes} {filename if filename else ''}")
 
 
 if __name__ == "__main__":
